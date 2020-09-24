be467e2812265210dead790a6ec9a583daaf9224
zahgenya <zahgenya@gmail.com>
2020-09-24 18:33:57 +0300

    task001 ready

--- task001.js
+++ task001.js
@@ -3,7 +3,19 @@
  * Пошук відбувається серед чисел списку, який передається першим аргументом. Якщо цей аргумент не є списоком чисел, то поверати null
  */
 function maxInArray(arr) {
-  // put here your solution
+  let max = null
+  if (Array.isArray(arr) && arr.length > 0) {
+  max = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] != "number") {
+          return null;
+        }
+        if (arr[i] > max) {
+          max = arr[i];
+        }
+    }
+  }
+  return max
 }
 
 module.exports = {maxInArray}
