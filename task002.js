c97751a35cce710467a591cc4a12ed7e710e5a77
Oleg Neizhkasha <oleg@attendify.com>
2020-09-26 15:05:53 +0300

    Added task002.js

---
+++ task002.js
@@ -0,0 +1,32 @@
+/**
+ * Test if `xs` is array of elements each of them is type of `type`
+ * @arg {array} xs
+ * @arg {string} type
+ * @return {boolean}
+ */
+const isArrayOf = (xs, type) => {
+
+}
+
+
+/**
+ * Filter array `xs` and return only elements which has type `type`
+ * @arg {array} xs
+ * @arg {string} type
+ * @return {array}
+ */
+const takeOf = (xs, type) => {
+
+}
+
+/**
+ * Filter array of numbers `xs` and return array without numbers which multiples `x`
+ * @arg {array} xs
+ * @arg {number} x
+ * @return {array}
+ */
+const filterMultiplesOf = (xs, x) => {
+
+}
+
+module.exports = {isArrayOf, filterMultiplesOf}
