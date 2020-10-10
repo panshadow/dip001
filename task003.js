4f3a5d2d2fa8130f1055cdfef24951389e73485f
Oleg Neizhkasha <oleg@attendify.com>
2020-10-10 13:29:10 +0300

    Added task003

---
+++ task003.js
@@ -0,0 +1,12 @@
+/**
+ * Generate range as array of number from `from` to `to` with step `step`
+ * @arg {number} from
+ * @arg {number} to
+ * @arg {number} [step=1]
+ * @return {number[]}
+ */
+function range(from,to,step=1) {
+
+}
+
+module.exports = {range};
