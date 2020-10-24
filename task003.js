451e6778812f2c1c46fc5e0de27c0a5f2f94233a
zahgenya <zahgenya@gmail.com>
2020-10-24 13:24:49 +0300

    New function range()

--- task003.js
+++ task003.js
@@ -6,7 +6,21 @@
  * @return {number[]}
  */
 function range(from,to,step=1) {
-
+  let result = [];
+  const check_fwd = (x, y) => x < y;
+  const check_bck = (x, y) => x > y;
+  let check;
+  if(from > to) {
+    check = check_bck;
+    step = Math.abs(step) * -1;
+  } else {
+    check = check_fwd;
+    step = Math.abs(step);
+  }
+  for (let x = from; check(x, to); x += step) {
+    result.push(x)
+  }
+  return result
 }
 
 module.exports = {range};
